47d5a9d7059a7828fbe256decc3c47ddc9575de0
Ned-el-ch <nedelchev.niki@gmail.com>
2020-04-08 16:12:46 +0100

    Done.

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -3,45 +3,45 @@ import { connect } from 'react-redux'
 
 class UserInput extends Component {
 
-  state = {
-    username: '',
-    hometown: ''
-  }
+	state = {
+		username: '',
+		hometown: ''
+	}
 
-  handleInputChange = (event) => {
-    this.setState({
-      [event.target.id]: event.target.value
-    });
-  }
+	handleInputChange = (event) => {
+		this.setState({
+			[event.target.id]: event.target.value
+		});
+	}
 
-  handleOnSubmit = (event) => {
-    event.preventDefault();
-    this.props.dispatch({type: 'ADD_USER', user: this.state})
-  }
+	handleOnSubmit = (event) => {
+		event.preventDefault();
+		this.props.dispatch({type: 'ADD_USER', user: this.state})
+	}
 
-  render() {
-    return(
-      <form onSubmit={this.handleOnSubmit}>
-        <p>
-          <input
-            type="text"
-            id="username"
-            onChange={this.handleInputChange}
-            placeholder="username"
-          />
-        </p>
-        <p>
-          <input
-            type="text"
-            id="hometown"
-            onChange={this.handleInputChange}
-            placeholder="hometown"
-          />
-        </p>
-        <input type="submit" />
-      </form>
-    )
-  }
+	render() {
+		return(
+			<form onSubmit={this.handleOnSubmit}>
+				<p>
+					<input
+						type="text"
+						id="username"
+						onChange={this.handleInputChange}
+						placeholder="username"
+					/>
+				</p>
+				<p>
+					<input
+						type="text"
+						id="hometown"
+						onChange={this.handleInputChange}
+						placeholder="hometown"
+					/>
+				</p>
+				<input type="submit" />
+			</form>
+		)
+	}
 }
 
 export default connect()(UserInput);
--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import App from './App'
 
 import manageUsers from './reducers/manageUsers'
 
+import { createStore } from "redux";
+import { Provider } from 'react-redux';
 
-// add imports and code
-
+const store = createStore(
+	manageUsers,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	);
 
 ReactDOM.render(
-  // add imports and code
-  <App />
-  // add imports and code
-  ,
-  document.getElementById('root')
+	<Provider store={store}>
+		<App />
+	</Provider>
+	,
+	document.getElementById('root')
 );
